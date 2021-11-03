edf9863e0be841a5ec9b89064db7f491a8827423
Sodgerel Mandakhnaran <66254521+leregdos@users.noreply.github.com>
2021-11-03 22:29:52 +0400

    Update nov4.ino

--- nov4/nov4.ino
+++ nov4/nov4.ino
@@ -1,20 +1,20 @@
-const int buttonSwitch = A0;
-const int greenPin = 10;
-const int bluePin = 13;
+const int buttonSwitch = A0; // saving the switch input into a variable
+const int greenPin = 10; // saving the green LED output into a variable
+const int bluePin = 13; // saving the blue LED output into a variable
 void setup() {
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT); // setting green LED for output
+  pinMode(bluePin, OUTPUT); // setting blue LED for output
 
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonSwitch);
+  int buttonState = digitalRead(buttonSwitch); // reading the switch on digital mode so it only has 2 states (ON and OFF)
 
-  if (buttonState == HIGH) {
+  if (buttonState == HIGH) { // if the switch is connected
     digitalWrite(greenPin, HIGH);
     digitalWrite(bluePin, LOW);
   }
-  else {
+  else {  // if the switch is disconnected
     digitalWrite(greenPin, LOW);
     digitalWrite(bluePin, HIGH);
   }
